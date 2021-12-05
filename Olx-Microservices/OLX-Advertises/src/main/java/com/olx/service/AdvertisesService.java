b55f7b6a22d1147b52e62f70edfcd397e230aac9
DHARMAVARAM SHIVAKRISHNA <67229657+ShivakrishnaDharmavaram@users.noreply.github.com>
2021-12-05 21:57:39 +0530

    Update AdvertisesService.java

--- Olx-Microservices/OLX-Advertises/src/main/java/com/olx/service/AdvertisesService.java
+++ Olx-Microservices/OLX-Advertises/src/main/java/com/olx/service/AdvertisesService.java
@@ -11,12 +11,9 @@ public interface AdvertisesService {
 	
 	public Advertise updateAdvertise(Advertise advertise, String authToken, int advertiseId);
 	public List<Advertise> getAllAdvertises(String authToken);
-	public Advertise getAdvertiseByUserPostId(String authToken, int postId);
-	public boolean deleteAdvertiseByPostId(String authToken, int postId);
-	public List<Advertise> getAdvertiseByCriteria(String searchText, String category, String postedBy,
-			String dateCondition, String onDate, String fromDate, String toDate, String sortBy,
-			int startIndex, int records);
-	public Advertise getAdvertiseByPostId(String authToken, int postId);
+	public Advertise getAdvertiseByUserId(String authToken, int postId);
+	public boolean deleteAdvertiseById(String authToken, int postId);
+	public Advertise getAdvertiseById(String authToken, int postId);
 	public List<Advertise> getAdvertiseBySearchText(String searchText);
 
 }
